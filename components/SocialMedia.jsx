b62d822134db77c5502fa5c8980c18e3beea55ed
carolinebelle <71263658+carolinebelle@users.noreply.github.com>
2022-08-04 14:11:03 -0400

    edits

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a
+          href={"https://www.linkedin.com/in/caroline-intuitive"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <FaFacebookF />
-      </div>
-      <div>
-        <BsInstagram />
+        <a
+          href={"https://github.com/carolinebelle"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsGithub />
+        </a>
       </div>
     </div>
   );
